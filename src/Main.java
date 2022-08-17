7b5b9e6ee7c852b69e83ffee0ef0cc7b6c1fa5f9
Mikhail <kamenevmm@gmail.com>
2022-08-17 21:41:45 +0300

    вроде готово

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Let's start!");
+        Author writer1 = new Author("Robert", "Stevenson");
+        Author writer2 = new Author("George", "Orwell");
+        Bk book1 = new Bk("Treasure island", writer1, 1883);
+        Bk book2 = new Bk("1984", writer2, 1949);
+        book1.setYear(1881);
+        System.out.println("Имя автора - " + writer1.getFullName());
+        System.out.println("Название книги - " + book1.getName() + ". Писатель - " + book1.getWriter().getFullName() + ". Издана в " + book1.getYear() + " году.");
+        System.out.println("Название книги - " + book2.getName() + ". Писатель - " + book2.getWriter().getFullName() + ". Издана в " + book2.getYear() + " году.");
     }
 }
